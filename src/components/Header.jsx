2387afd60bee6723ac726498dc02e6b92a5944a6
Ahana Kulkarni <ahanapk2319@gmail.com>
2026-07-26 19:13:22 +0530

    Update logo, favicon, header transparency, and doctor photo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,12 +17,9 @@ const Header = () => {
       <div className="header container">
         <div className="header-inner">
         <div className="logo">
-          <div className="logo-icon">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6 9L12 15L18 9" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
-          </div>
-          <span className="logo-text">Bonafide</span>
+          <a href="/">
+            <img src="/logo.png" alt="Bonafide Dental Clinic" className="logo-image" />
+          </a>
         </div>
         
         <nav className="nav-links">
