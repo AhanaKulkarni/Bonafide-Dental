f9c1385d4fd7abb84d29812ceef8d26cf9045382
Ahana Kulkarni <ahanapk2319@gmail.com>
2026-08-05 23:11:32 +0530

    Pixel perfect tweaks: Floating rounded navbar, navy buttons, exact valley wave SVG, brighter tooth blue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ const Header = () => {
           <a href="#book">Book Appointment</a>
         </nav>
         
-        <a href="https://wa.me/918097140166" target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+        <a href="https://wa.me/918097140166" target="_blank" rel="noopener noreferrer" className="btn btn-navy">
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
           </svg>
